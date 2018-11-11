9582a7b3fc2c922eae7c1b3b1565c14595a3c3ec
Daniel Castro Vilela <danielcastrovilela@hotmail.com>
2018-11-10 22:18:57 -0300

    Tabela Cliente e alterações classe cadastrar_cliente

--- Projeto/app/src/main/java/com/example/brunab/projeto/dao/database.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/dao/database.java
@@ -7,12 +7,25 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class database extends SQLiteOpenHelper{
 
 
-    public database(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public database(Context context){
+        super(context,"BancoDB",null,1);
     }
+
+
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql= "CREATE TABLE IF NOT EXISTS CLIENTES(" +
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "NOME VARCHAR(30)," +
+                "CPF VARCHAR(11)," +
+                "ENDERECO VARCHAR(50),"+
+                "DEPENDENTES INTEGER,"+
+                "CNH VARCHAR(13));";
+
+        sqLiteDatabase.execSQL(sql);
+
+
 
     }
 
--- Projeto/app/src/main/java/com/example/brunab/projeto/telas/cadastrar_cliente.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/telas/cadastrar_cliente.java
@@ -2,14 +2,31 @@ package com.example.brunab.projeto.telas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.brunab.projeto.R;
+import com.example.brunab.projeto.entidade.Cliente;
 
 public class cadastrar_cliente extends AppCompatActivity {
+     private Button btSalvar;
+     private Button btCancelar;
+     private Cliente cliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_cliente);
+        btCancelar = findViewById(R.id.btCancelar);
+        btSalvar = findViewById(R.id.btSalvar);
+
+    }
+
+    public void acaoCancelar(View v){
+        finish();
+    }
+
+    public void acaoSalvar(){
+
     }
 }
