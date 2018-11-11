a55ead61e20b2dfc0d4e5ebcc4b62a886cb3e1e1
joão bezerra <job_silvaneto@hotmail.com>
2018-11-10 22:22:48 -0300

    chamando tela cadastro

--- Projeto/app/src/main/java/com/example/brunab/projeto/telas/MainActivity.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/telas/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.brunab.projeto.telas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.brunab.projeto.R;
 
@@ -11,6 +13,10 @@ public class    MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void cadastracliente(View view){
+        Intent it = new Intent(MainActivity.this,cadastrar_cliente.class);
+        startActivity(it);
     }
 
 }
