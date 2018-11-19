75f0fa32e063462330ef3d1d11df5d1f65b47db9
WesllayOSantos <wesllayosantos@gmail.com>
2018-11-19 14:38:43 -0300

    arquivo banco

--- Projeto/app/src/main/java/com/example/brunab/projeto/dao/database.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/dao/database.java
@@ -15,13 +15,59 @@ public class database extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql= "CREATE TABLE IF NOT EXISTS CLIENTES(" +
-                "ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-                "NOME VARCHAR(30)," +
-                "CPF VARCHAR(11)," +
-                "ENDERECO VARCHAR(50),"+
-                "DEPENDENTES INTEGER,"+
-                "CNH VARCHAR(13));";
+        String sql= "CREATE TABLE 'Carro' (\n" +
+                "  'IDCARRO' INT NOT NULL,\n" +
+                "  'NOME' VARCHAR(40) NULL,\n" +
+                "  'MARCA' VARCHAR(40) NULL,  \n" +
+                "  'MODELO' VARCHAR(40) NULL,\n" +
+                "  'VALORDOSEGURO' FLOAT,  \n" +
+                "  'VALORDALOCACAO' FLOAT,  \n" +
+                "  'COR' VARCHAR(40) NULL,\n" +
+                "  'ATIVO' BOOLEAN,\n" +
+                "   PRIMARY KEY ('IDCARRO'));\n" +
+
+                "CREATE TABLE 'Cliente' (\n" +
+                "  'IDCLIENTE' INT NOT NULL,\n" +
+                "  'RG' VARCHAR(11) NULL,\n" +
+                "  'CPF' VARCHAR(11) NULL,\n" +
+                "  'NOME' VARCHAR(40) NULL,\n" +
+                "  'ENDERECO' VARCHAR(50) NULL,\n" +
+                "  'CNH' VARCHAR(11) NULL,\n" +
+                "  'NUMERODEDEPENDENTES' INT NULL,\n" +
+                "  PRIMARY KEY ('IDCLIENTE'));\n" +
+
+                "CREATE TABLE 'Funcionario' (\n" +
+                "  'IDFUNCIONARIO' INT NOT NULL,\n" +
+                "  'RG' VARCHAR(11) NULL,\n" +
+                "  'CPF' VARCHAR(11) NULL,\n" +
+                "  'NOME' VARCHAR(40) NULL,\n" +
+                "  'ENDERECO' VARCHAR(50) NULL,\n" +
+                "  'DATADEADMISSAO' DATE NULL,\n" +
+                "  'DATADEDEMISSAO' DATE NULL,\n" +
+                "  'SUPERVISOR' BOOLEAN,\n" +
+                "  PRIMARY KEY ('IDFUNCIONARIO'));\n" +
+
+                "CREATE TABLE 'Locacao' (\n" +
+                "  'IDLOCACAO' INT NOT NULL,\n" +
+                "  'NOME' VARCHAR(40) NULL,\n" +
+                "  'MARCA' VARCHAR(40) NULL,  \n" +
+                "  'MODELO' VARCHAR(40) NULL,\n" +
+                "  'VALORDOSEGURO' FLOAT,  \n" +
+                "  'VALORDALOCACAO' FLOAT,  \n" +
+                "  'COR' VARCHAR(40) NULL,\n" +
+                "  'ATIVO' BOOLEAN,\n" +
+                "  'DATADELOCACAO' DATE NULL,\n" +
+                "  'DATADEDEVOLUCAO' DATE NULL,\n" +
+                "  'QUILOMETRAGEM' FLOAT, \n" +
+                "   PRIMARY KEY ('IDLOCACAO'));\n" +
+
+                "CREATE TABLE 'Pessoa' (\n" +
+                "  'IDPESSOA' INT NOT NULL,\n" +
+                "  'RG' VARCHAR(11) NULL,\n" +
+                "  'CPF' VARCHAR(11) NULL,\n" +
+                "  'NOME' VARCHAR(40) NULL,\n" +
+                "  'ENDERECO' VARCHAR(50) NULL,\n" +
+                "  PRIMARY KEY ('IDPESSOA'));";
 
         sqLiteDatabase.execSQL(sql);
 
