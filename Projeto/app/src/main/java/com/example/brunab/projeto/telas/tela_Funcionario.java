ab1b9039c02d6ab54fdb7f870580689f7c309b8e
wmirandaa <willian_mirandaantonio@hotmail.com>
2018-11-20 01:27:11 -0300

    atualizacoes tela funcionario

--- Projeto/app/src/main/java/com/example/brunab/projeto/telas/tela_Funcionario.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/telas/tela_Funcionario.java
@@ -2,6 +2,7 @@ package com.example.brunab.projeto.telas;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.brunab.projeto.R;
+import com.example.brunab.projeto.dao.database;
 import com.example.brunab.projeto.entidade.Funcionario;
 import com.example.brunab.projeto.telas.MainActivity;
 
@@ -20,10 +22,15 @@ public class tela_Funcionario extends AppCompatActivity {
     EditText edEndereco;
     EditText edCargo;
 
+    private SQLiteDatabase db;
+    database daoBD;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_funcionario);
+        daoBD = new database(this);
+        db=daoBD.getWritableDatabase();
         edNome = findViewById(R.id.edNomeFuncionario);
         edCpf = findViewById(R.id.edCPFfuncionario);
         edRG = findViewById(R.id.edRGfuncionario);
@@ -37,18 +44,22 @@ public class tela_Funcionario extends AppCompatActivity {
     }
 
 
-    public void acaoSalvar(){
+    public void acaoSalvar(View view){
     funcionario.setNome(edNome.getText().toString());
     funcionario.setCpf(edCpf.getText().toString());
     funcionario.setEndereco(edEndereco.getText().toString());
     funcionario.setRg(edRG.getText().toString());
-
+    inserir();
+    finish();
     }
 
     public void inserir(){
         ContentValues campos= new ContentValues();
         campos.put("NOME",funcionario.getNome());
         campos.put("RG",funcionario.getRg());
+        campos.put("CPF",funcionario.getCpf());
+        campos.put("ENDERECO",funcionario.getEndereco());
+
     }
 
 
