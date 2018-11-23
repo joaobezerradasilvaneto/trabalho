126b6c339b1ffd409243d17c5e036439c8e02ab8
joão bezerra <job_silvaneto@hotmail.com>
2018-11-23 19:44:58 -0300

    tela de listagem

---
+++ Projeto/app/src/main/java/com/example/brunab/projeto/telas/listarlocacao.java
@@ -0,0 +1,15 @@
+package com.example.brunab.projeto.telas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.brunab.projeto.R;
+
+public class listarlocacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listarlocacao);
+    }
+}
